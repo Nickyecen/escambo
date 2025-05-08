0059c3fad01fdd0d2649718ec51ab2421ebe273a
Nickyecen <nicolasecendron@gmail.com>
2025-05-08 14:47:20 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# e-Scambo
+Projeto de Engenharia de Software desenvolvido pelos alunos André da Silva Frainer, João Speranza Pastorello, Leonardo Barros Bilhalva, Nathan Rambo Prediger e Nicolas Ebone Cendron
---
+++ src/test/java/br/com/escambo/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.escambo.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
